ca064614a4f4768d150a9cb1b2a51bf4e0adb822
Simon Porritt <simon@jsplumbtoolkit.com>
2023-02-06 22:43:23 +1100

    initial import

---
+++ App.svelte
@@ -0,0 +1,24 @@
+
+<script>
+    import {onMount} from "svelte"
+    import {MindmapComponent, InspectorComponent} from "@jsplumb-components/mindmap-svelte"
+
+    let mindmapBuilder
+
+    onMount(async () => {
+        mindmapBuilder.loadUrl("./mindmap-1.json", () => {
+            mindmapBuilder.zoomToFit()
+        })
+    })
+
+</script>
+
+
+<div id="canvas">
+    <MindmapComponent id="myMindmap" bind:this={mindmapBuilder}/>
+</div>
+
+<div id="rhs">
+    <InspectorComponent mindmapId="myMindmap"></InspectorComponent>
+</div>
+
